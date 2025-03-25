017d937c53083bebe8082b5b1f1848635a1c85bc
unknown <YY341@ccs.local>
2025-03-25 19:42:46 +0800

    listing feature

--- rest-api-file-main/REST-API-FILE-main/src/products/products.routes.ts
+++ rest-api-file-main/REST-API-FILE-main/src/products/products.routes.ts
@@ -18,3 +18,29 @@ productRouter.post("/product", async (req: Request, res: Response) => {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: (error as Error).message });
     }
 });
+// Get all products
+
+productRouter.get("/products", async (req: Request, res: Response) => {
+    try {
+        const allProducts = await database.findAll();
+        if (allProducts.length === 0) {
+            return res.status(StatusCodes.NOT_FOUND).json({ error: "No products found!" });
+        }
+        return res.status(StatusCodes.OK).json({ total: allProducts.length, allProducts });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: (error as Error).message });
+    }
+});
+
+// Get a single product by ID
+productRouter.get("/products/:id", async (req: Request, res: Response) => {
+    try {
+        const product = await database.findOne(req.params.id);
+        if (!product) {
+            return res.status(StatusCodes.NOT_FOUND).json({ error: "Product does not exist" });
+        }
+        return res.status(StatusCodes.OK).json({ product });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: (error as Error).message });
+    }
+});
